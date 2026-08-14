26349b9d0348a60eb7ef954448ec37ded24ad4b4
Syed Muhammad Ammar <syedmammar123@gmail.com>
2026-08-15 01:59:30 +0500

    Add render configuration and initialize worker job processing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { app } from './app';
 import { config } from './config';
 import { logger } from './lib/logger';
+import './jobs/worker';
 
 app.listen(config.port, () => {
   logger.info(`Server listening on port ${config.port}`);
