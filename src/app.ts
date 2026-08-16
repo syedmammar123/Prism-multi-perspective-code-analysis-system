8dbb332f034601f8ae98adbbf47d3460b61f35cb
Syed Muhammad Ammar <syedmammar123@gmail.com>
2026-08-17 02:07:03 +0500

    landing page and github workflow add

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import path from 'path';
 import pinoHttp from 'pino-http';
 import { logger } from './lib/logger';
 import { healthRoute } from './api/routes/health.route';
 import { webhookRoute } from './api/routes/webhook.route';
 
 export const app = express();
+
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(
   express.json({
